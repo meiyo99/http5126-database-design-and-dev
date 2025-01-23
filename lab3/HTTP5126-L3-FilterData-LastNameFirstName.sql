dec7d9f96f5bac560944ba314fd30a0e062a611a
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-01-23 17:46:07 -0500

    added answers to sql file

--- lab3/HTTP5126-L3-FilterData-LastNameFirstName.sql
+++ lab3/HTTP5126-L3-FilterData-LastNameFirstName.sql
@@ -3,29 +3,30 @@
 
 --  1 
 -- A
-
+SELECT * FROM employee WHERE role = 'Manager' OR role = 'Assistant';
 -- B
-
+SELECT * FROM stock_item WHERE category = 'Piscine' AND inventory < 24;
 
 --  2
 -- A
-
+SELECT item, price FROM stock_item WHERE item LIKE '%cage';
 -- B
-
+SELECT first_name FROM employee WHERE first_name LIKE 'F%';
 
 --  3
 -- A
-
+SELECT * FROM stock_item WHERE stock_item_id BETWEEN 1010 AND 1015;
 -- B
-
+SELECT item FROM stock_item WHERE price BETWEEN 10 AND 20;
 
 --  4
 -- A
-
+SELECT CONCAT(first_name, " ", last_name) AS 'name', phone FROM employee WHERE NOT (role = 'Manager' OR role = 'Assistant');
 -- B
-
+SELECT item, price, inventory FROM stock_item WHERE NOT (category = 'Feline' OR category = 'Canine' OR inventory > 20);
 
 --  5
 -- A
-
+SELECT first_name, last_name, role AS Job Title, phone FROM employee ORDER BY last_name;
 -- B
+SELECT stock_item_id, item, price, inventory FROM stock_item WHERE category = 'Murine' AND inventory > 20 ORDER BY price DESC;
