eafb68503079ab2ed115a54b65fe10f97d9b3571
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-02-06 16:10:33 -0500

    added lab5 files

---
+++ week5/lab5/HTTP5126-L5-MultipleTables-LastNameFirstName.sql
@@ -0,0 +1,36 @@
+--  LAB 5 MULTIPLE TABLES
+
+--  Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 5; your query for question 1B should go on line 7...
+--  1 
+-- A 
+
+-- B 
+
+
+--  2
+-- A 
+
+-- B
+
+
+--  3
+-- A
+
+-- B
+
+
+--  4
+-- A
+
+-- B
+
+
+--  5
+-- A
+
+-- B
+
+
+--  6
+-- A 
+-- B
