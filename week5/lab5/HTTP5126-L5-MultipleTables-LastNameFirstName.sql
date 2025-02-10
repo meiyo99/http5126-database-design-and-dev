b96a90e18b3d0767054704adaf41e7e9b0380584
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-02-10 11:41:48 -0500

    changed sql file

--- week5/lab5/HTTP5126-L5-MultipleTables-LastNameFirstName.sql
+++ week5/lab5/HTTP5126-L5-MultipleTables-LastNameFirstName.sql
@@ -3,7 +3,7 @@
 --  Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 5; your query for question 1B should go on line 7...
 --  1 
 -- A 
-
+SELECT * FROM sale WHERE stock_item_id = 1014;
 -- B 
 
 
