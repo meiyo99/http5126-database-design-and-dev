41bf81d434b0ac721f66ec0d518220fe1b210be9
Mayuresh Naidu <mayureshnaiduu@gmail.com>
2025-03-03 20:57:36 -0500

    added week8 file

---
+++ week8/HTTP5126-L8-CRUD-LastNameFirstName.sql
@@ -0,0 +1,36 @@
+--  LAB 8 CRUD
+--  Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 6; your query for question 1B should go on line 7...
+
+-- 1 
+--  A 
+
+--  B 
+
+--  C 
+
+--  D
+
+--  E 
+
+
+-- 2 
+--  A 
+
+--  B 
+
+--  C 
+
+--  D
+
+--  E 
+
+
+-- 3 
+--  A 
+
+--  B 
+
+-- 4 
+--  A 
+
+--  B 
